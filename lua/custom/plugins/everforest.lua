c2d6d0e71f462e11cd0185942009e8e5a0652b39
cateperson <197832094+cateperson@users.noreply.github.com>
2025-12-15 10:03:56 +0000

    fixed transparency issues

--- lua/custom/plugins/everforest.lua
+++ lua/custom/plugins/everforest.lua
@@ -5,6 +5,7 @@ return {
     priority = 1000,
     config = function()
       require('everforest').setup {
+        transparent_background_level = 1,
         -- background = 'soft', -- Options: 'hard', 'medium', 'soft'
       }
     end,
